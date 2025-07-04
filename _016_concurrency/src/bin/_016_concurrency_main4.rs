472782a6bec10794fa9977692709ad3daf766ad8
Amir Hossein Pakdaman aka SSENERG <amirhossein.pdn@gmail.com>
2025-07-05 02:21:16 +0330

    chapter 16 is done

---
+++ _016_concurrency/src/bin/_016_concurrency_main4.rs
@@ -0,0 +1,23 @@
+use std::sync::{Arc, Mutex};
+use std::thread;
+
+fn main() {
+    let counter = Arc::new(Mutex::new(0));
+
+    // Create a scope for the threads
+    thread::scope(|s| {
+        // Spawn 100 threads
+        for _ in 0..10_000 {
+            let counter = Arc::clone(&counter);
+                              // #############
+            s.spawn(move || { // ### IMPORTANT
+                              // ############# 
+                let mut num = counter.lock().unwrap();
+                *num += 1;
+            });
+        }
+        // All threads are automatically joined when the scope ends
+    });
+
+    println!("Result: {}", *counter.lock().unwrap());
+}
